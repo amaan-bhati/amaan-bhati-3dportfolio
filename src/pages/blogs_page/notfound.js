a2f79930e8d8d63879e5f2f09c9819e1cab9087c
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-12-21 23:20:07 +0530

    error message for unloaded pages

--- src/pages/blogs_page/notfound.js
+++ src/pages/blogs_page/notfound.js
@@ -5,23 +5,23 @@ import { Container } from "react-bootstrap";
 export default function Notfound() {
   return (
     <section className="home-section">
-      <Container fluid id="home">
-        <Particle />
+    <Container fluid id="home">
+    <Particle />
 
-        <Container className="home-content d-flex flex-column justify-content-center align-items-center comingsoonclass">
-          <h1 style={{ marginBottom: "20px" }}>
-            Stay tuned, this page will be updated soon.
-          </h1>
-          <lottie-player
-            src="https://assets6.lottiefiles.com/packages/lf20_zkzzfkke.json"
-            background="transparent"
-            speed="1"
-            style={{ width: "40%", height: "100%" }}
-            loop
-            autoplay
-          ></lottie-player>
-        </Container>
-      </Container>
+    <Container className="home-content d-flex flex-column justify-content-center align-items-center comingsoonclass">
+      <h1 style={{ marginBottom: "20px" }}>
+        Stay tuned, this page will be updated soon.
+      </h1>
+      <lottie-player
+        src="https://assets6.lottiefiles.com/packages/lf20_zkzzfkke.json"
+        background="transparent"
+        speed="1"
+        style={{ width: "40%", height: "100%" }}
+        loop
+        autoplay
+      ></lottie-player>
+    </Container>
+  </Container>
     </section>
   );
 }
