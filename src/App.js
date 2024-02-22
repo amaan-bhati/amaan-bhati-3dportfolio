fe36c2dbc604033c0b5f82582f5d23de2c5e9ed1
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-02-23 00:53:15 +0530

    post deployement updates

--- src/App.js
+++ src/App.js
@@ -62,8 +62,9 @@ function App() {
             <Route path="socialmedia" element={<SocialMedia />}></Route>
             <Route path="toolkit" element={<Toolkit />}></Route>
           </Route>
-          <Route path="/projectspage" element={<ProjectPage />} />
-          <Route path="/certificatepage" element={<CertificatePage />} />
+          {/*<Route path="/projectspage" element={<ProjectPage />} />*/}
+          <Route path="/projectspage" element={<Notfound />} />
+        {/*  <Route path="/certificatepage" element={<CertificatePage />} />*/}
           <Route path="/resume" element={<Resume />} />
           <Route path="/blogs" element={<ComingSoon />} />
           <Route path="*" element={<Notfound />} />
